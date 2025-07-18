154288a1cb43c78462b17b9c60554be9c634c9e8
Luu Hanh <luuhanh0201@gmail.com>
2025-07-18 16:38:11 +0700

    add port db

--- src/config/db.js
+++ src/config/db.js
@@ -7,4 +7,5 @@ export const connection = await mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port:process.env.DB_PORT
 });
